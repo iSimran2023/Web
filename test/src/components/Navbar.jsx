d52368717b5759cfa1e0494bd68f0dad1d9702c5
SimranPun <simran.mqr111@gmail.com>
2025-06-24 15:33:45 +0545

    Added

---
+++ test/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Grid,
+Box
+} from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <Grid>
+        <Box>
+            
+        </Box>
+    </Grid>
+  )
+};
+
+export default Navbar
