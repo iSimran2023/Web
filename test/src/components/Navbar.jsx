b9396873486be92f9f8ce346711dccbd20f4f48a
SimranPun <simran.mqr111@gmail.com>
2025-06-25 23:00:01 +0545

    changes in navbar

--- test/src/components/Navbar.jsx
+++ test/src/components/Navbar.jsx
@@ -1,264 +1,231 @@
-import React from "react";
-import { Grid, Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Grid,
+  Box,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md")); // True if screen is small
+  const [drawerOpen, setDrawerOpen] = useState(false); // Controls mobile drawer visibility
+
   return (
     <>
-      <Grid item xs={12} sm={3} sx={{ display: "flex", gap: 2 }}>
+      {/* ---------- Top Header Section with Logos and Campus Info ---------- */}
+      <Grid
+        item
+        xs={12}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: { xs: "column", md: "row" },
+          p: 1,
+          textAlign: { xs: "center", md: "left" },
+        }}
+      >
+        {/* Left Logo */}
         <Box
           component="img"
           src="TU.jpg"
           alt="Tribhuvan University Logo"
           sx={{
-            width: "100px",
-            height: "100px",
-            marginLeft: "100px",
+            width: { xs: 130, md: 130 },
+            height: { xs: 130, md: 130 },
+            mb: { xs: 1, md: 0 },
+            ml: { xs: 0, md: "100px" },
           }}
         />
 
+        {/* Center Text */}
         <Box
           sx={{
-            mx: "auto",
-            textAlign: "center",
             color: "#024282",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            height: "100%",
-            mt: 0,
+            textAlign: "center",
+            flex: 1,
           }}
         >
-          <Typography variant="body2" sx={{ fontSize: { lg: "0.75rem" } }}>
+          <Typography variant="body2">
             त्रिभुवन विश्वविद्यालयबाट सम्बन्धन प्राप्त
           </Typography>
-          <Typography variant="body2" sx={{ fontSize: { lg: "0.75rem" } }}>
+          <Typography variant="body2">
             Affiliated to Tribhuwan University
           </Typography>
-
-          <Typography
-            variant="h4"
-            component="h2"
-            sx={{
-              fontWeight: "bold",
-              fontSize: { xs: "1rem", lg: "1rem" },
-              fontFamily: "'Noto Sans Devanagari', sans-serif",
-            }}
-          >
+          <Typography variant="h6" sx={{ fontWeight: "bold" }}>
             नेपालगञ्ज बहुमुखी क्याम्पस
           </Typography>
-          <Typography
-            variant="h4"
-            component="h2"
-            sx={{
-              fontWeight: "bold",
-              fontSize: { xs: "1rem", lg: "1rem" },
-            }}
-          >
+          <Typography variant="h6" sx={{ fontWeight: "bold" }}>
             NEPALGUNJ MULTIPLE CAMPUS
           </Typography>
-
-          <Typography variant="body2" sx={{ fontSize: { lg: "0.75rem" } }}>
-            नेपालगञ्ज, बाँके
-          </Typography>
-          <Typography variant="body2" sx={{ fontSize: { lg: "0.75rem" } }}>
-            Nepalgunj, Banke
-          </Typography>
+          <Typography variant="body2">नेपालगञ्ज, बाँके</Typography>
+          <Typography variant="body2">Nepalgunj, Banke</Typography>
         </Box>
 
+        {/* Right Flag */}
         <Box
           component="img"
           src="https://media.tenor.com/MCKjaHTU0kwAAAAj/nepal.gif"
-          alt="Nepal Flag Animation"
+          alt="Nepal Flag"
           sx={{
-            width: "100px",
-            height: "100px",
+            display: { xs: "none", md: "block" },
+            width: 130,
+            height: 130,
             marginRight: "100px",
           }}
         />
       </Grid>
 
-      <Grid container>
-        <Grid
-          item
-          xs={12}
-          sx={{
-            display: "flex",
-            height: "45px",
-            backgroundColor: "rgb(17, 105, 191)",
-            width: "100%",
-            mt: "7px",
-            alignItems: "center",
-            justifyContent: "space-between",
-            px: "100px",
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              gap: 6,
-              flexWrap: "wrap",
-              overflowX: "auto",
-            }}
-          >
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              Home
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              About us
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              Contents
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              Programs
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              Reports
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              News and Events
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              Downloads
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              Curriculum
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              Gallery
-            </Box>
-            <Box
-              component="a"
-              href="#"
-              sx={{
-                color: "white",
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                "&:hover": {
-                  color: "#F58D4C",
-                },
-              }}
-            >
-              Contact us
-            </Box>
-          </Box>
+      {/* ---------- Navigation Bar Section ---------- */}
+<Box sx={{ 
+  width: '100%', 
+  backgroundColor: "rgb(17, 105, 191)", 
+  overflow: 'hidden',
+  height: "45px"
+}}>
+  <Box sx={{
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    px: { xs: 2, md: 10, lg: "150px" },
+    maxWidth: '100%',
+    margin: '0 auto',
+    overflowX: 'auto',
+    scrollbarWidth: 'none',
+    '&::-webkit-scrollbar': { display: 'none' }
+  }}>
+   
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>Home</Box>
 
-          <Box
-            component="a"
-            href="#"
-            sx={{
-              color: "white",
-              textDecoration: "none",
-              "&:hover": {
-                color: "#F58D4C",
-              },
-              marginLeft: "auto",
-              pl: 2,
-            }}
-          >
-            Login
-          </Box>
-        </Grid>
-      </Grid>
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>About us</Box>
+
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>Contents</Box>
+
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>Programs</Box>
+
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>Reports</Box>
+
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>News and Events</Box>
+
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>Downloads</Box>
+
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>Curriculum</Box>
+
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>Gallery</Box>
+
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      whiteSpace: "nowrap",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      mr: 6,
+      "&:hover": { color: "#F58D4C" },
+    }}>Contact us</Box>
+
+    {/* Login Link */}
+    <Box component="a" href="#" sx={{
+      color: "white",
+      textDecoration: "none",
+      fontSize: "1.05rem",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      flexShrink: 0,
+      marginLeft: 'auto',
+      pl: 2,
+      "&:hover": { color: "#F58D4C" },
+    }}>Login</Box>
+  </Box>
+</Box>
     </>
   );
 };
